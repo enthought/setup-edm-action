9ac9644748b71c58b3f8403d2ff5f0b2f8f7fc23
Ioannis Tziakos <ioannist@enthought.com>
2025-03-28 13:17:37 +0000

    Update edm setup scripts to support the latest edm installers (#16)
    * Update edm setup scripts to support the latest edm installers
    * install edm 4.1.0 on the ci
    * Compile action

--- dist/install-edm-osx.sh
+++ dist/install-edm-osx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# (C) Copyright 2020 Enthought, Inc., Austin, TX
+# (C) Copyright 2020-2025 Enthought, Inc., Austin, TX
 # All rights reserved.
 #
 # This software is provided without warranty under the terms of the BSD
@@ -21,14 +21,24 @@ INSTALL_EDM_VERSION=$1
 DOWNLOAD_DIR=$2
 
 install_edm() {
+    local EDM_MAJOR="$(echo "$INSTALL_EDM_VERSION" | sed -E -e 's/([[:digit:]]+)\..*/\1/')"
     local EDM_MAJOR_MINOR="$(echo "$INSTALL_EDM_VERSION" | sed -E -e 's/([[:digit:]]+\.[[:digit:]]+)\..*/\1/')"
-    local EDM_PACKAGE="edm_cli_${INSTALL_EDM_VERSION}.pkg"
-    local EDM_INSTALLER_PATH="${DOWNLOAD_DIR}/${EDM_PACKAGE}"
+    local EDM_PACKAGE="edm_cli_${INSTALL_EDM_VERSION}"
+    local EDM_INSTALLER_PATH="${DOWNLOAD_DIR}/${EDM_PACKAGE}.pkg"
 
-    echo "https://package-data.enthought.com/edm/osx_x86_64/${EDM_MAJOR_MINOR}/${EDM_PACKAGE}"
 
+    # We special case some versions because the installer names have changed
+    if [ "$EDM_MAJOR_MINOR" == "4.0" ]; then
+        local EDM_URL="https://package-data.enthought.com/edm/osx_x86_64/${EDM_MAJOR_MINOR}/${EDM_PACKAGE}.pkg"
+    elif [ "$EDM_MAJOR" -ge "4" ]; then
+	local EDM_URL="https://package-data.enthought.com/edm/osx_x86_64/${EDM_MAJOR_MINOR}/${EDM_PACKAGE}_osx_x86_64.pkg"
+    else
+        local EDM_URL="https://package-data.enthought.com/edm/osx_x86_64/${EDM_MAJOR_MINOR}/${EDM_PACKAGE}.pkg"
+    fi
+
+    echo "$EDM_URL"
     if [ ! -e "$EDM_INSTALLER_PATH" ]; then
-        curl --fail --show-error -o "$EDM_INSTALLER_PATH" -L "https://package-data.enthought.com/edm/osx_x86_64/${EDM_MAJOR_MINOR}/${EDM_PACKAGE}"
+        curl --fail --show-error -o "$EDM_INSTALLER_PATH" -L "$EDM_URL"
     fi
 
     sudo installer -pkg "$EDM_INSTALLER_PATH" -target /
--- src/install-edm-windows.cmd
+++ src/install-edm-windows.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-:: (C) Copyright 2020 Enthought, Inc., Austin, TX
+:: (C) Copyright 2020-2025 Enthought, Inc., Austin, TX
 :: All rights reserved.
 ::
 :: This software is provided without warranty under the terms of the BSD
@@ -26,9 +26,20 @@ FOR /F "tokens=1,2,3 delims=." %%a in ("%INSTALL_EDM_VERSION%") do (
 )
 
 SET EDM_MAJOR_MINOR=%MAJOR%.%MINOR%
-SET EDM_PACKAGE=edm_cli_%INSTALL_EDM_VERSION%_x86_64.msi
-SET EDM_INSTALLER_PATH=%DOWNLOAD_DIR%\%EDM_PACKAGE%
-SET COMMAND="(new-object net.webclient).DownloadFile('https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%', '%EDM_INSTALLER_PATH%')"
+SET EDM_PACKAGE=edm_cli_%INSTALL_EDM_VERSION%
+SET EDM_INSTALLER_PATH=%DOWNLOAD_DIR%\%EDM_PACKAGE%.msi
+
+rem We special case the installer name because they changed after 4.1.0
+IF /I %EDM_MAJOR_MINOR%==4.0 (
+   SET EDM_URL=https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%_x86_64.msi
+) ELSE ( IF /I %MAJOR% GEQ 4 (
+   SET EDM_URL=https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%_win_x86_64.msi
+) ELSE (
+   SET EDM_URL=https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%_x86_64.msi
+))
+
+@ECHO.%EDM_URL%
+SET COMMAND="(new-object net.webclient).DownloadFile('%EDM_URL%', '%EDM_INSTALLER_PATH%')"
 
 IF NOT EXIST %EDM_INSTALLER_PATH% CALL powershell.exe -Command %COMMAND% || GOTO error
 CALL msiexec /qn /i %EDM_INSTALLER_PATH% EDMAPPDIR=C:\Enthought\edm || GOTO error
